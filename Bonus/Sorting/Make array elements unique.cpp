fdc9121a86a33f95b956bc6f1e6a2b5b939d894e
Harshita Khare <121930563+HarshitaKhare28@users.noreply.github.com>
2025-08-05 19:06:30 +0530

    Bonus Questions:Sorting

---
+++ Bonus/Sorting/Make array elements unique.cpp
@@ -0,0 +1,18 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+class Solution {
+    public:
+      int minIncrements(vector<int>& arr) {
+          // Code here
+          sort(arr.begin(),arr.end());
+          int cnt = 0;
+          for(int i = 1;i<arr.size();i++){
+              if(arr[i] <= arr[i-1]){
+                  cnt += arr[i-1]+1 - arr[i];
+                  arr[i] = arr[i-1] + 1;
+              }
+          }
+          return cnt;
+      }
+  };
